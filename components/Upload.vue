03cd030530622793d355f7f11fc4a7966e4b1997
bangbang6 <577174667@qq.com>
2021-03-29 10:36:09 +0800

    feat: 封装组件

--- components/Upload.vue
+++ components/Upload.vue
@@ -2,13 +2,13 @@
   <div class="user-center">
     <div ref="drag" class="drag">
       <!-- eslint-disable-next-line -->
-      <input type="file" name="file" @change="handleFileChange" class="input" />
+      <input type="file" name="file" @change="handleFileChange" class="input" ref="input" />
     </div>
     <div class="progress">
       文件上传
       <el-progress :text-inside="true" :stroke-width="20" :percentage="percentage" />
     </div>
-    <div class="cube-progress" :style="{width:cubeWidth+'px'}">
+    <div v-if="showCubeProcess" class="cube-progress" :style="{width:cubeWidth+'px'}">
       <div v-for="chunk in chunks" :key="chunk.name" class="cube">
         <div
           :style="{height:chunk.process+'%'}"
@@ -26,40 +26,81 @@
         </div>
       </div>
     </div>
-    <div class="progress">
+    <div v-if="showHashProcess" class="progress">
       计算hash
       <el-progress :text-inside="true" :stroke-width="20" :percentage="process" />
     </div>
-    <div class="progress">
-      计算hash2
-      <el-progress :text-inside="true" :stroke-width="20" :percentage="process2" />
-    </div>
-    <div class="progress">
-      计算hash3
-      <el-progress :text-inside="true" :stroke-width="20" :percentage="process3" />
-    </div>
+
     <button @click="uploadFile">上传</button>
+    <button v-if="!cancelStatus" @click="cancel">取消</button>
+    <button v-else @click="uploadAgain">重新上传</button>
   </div>
 </template>
 
 <script>
-import sparkMD5 from 'spark-md5'
-const CHUNK_SIZE = 0.5 * 1024 * 1024 // 0.5M 必须是整数
+import sparkMD5 from 'spark-md5' // 0.5M 必须是整数
+import axios from 'axios'
+
 export default {
+  props: {
+    uploadAction: {
+      type: String,
+      default: '/uploadfile'
+    },
+    checkFileAction: {
+      type: String,
+      default: '/checkFile'
+    },
+    mergeAction: {
+      type: String,
+      default: '/merge'
+    },
+    showHashProcess: {
+      type: Boolean,
+      default: true
+    },
+    showCubeProcess: {
+      type: Boolean,
+      default: true
+    },
+    hashType: {
+      type: Number,
+      default: 3
+    },
+    typeLimit: {
+      type: Boolean,
+      default: false
+    },
+    isCancel: {
+      type: Boolean,
+      default: true
+    },
+    errorLimit: {
+      type: Number,
+      default: 3
+    },
+    paralleLimit: {
+      type: Number,
+      default: 4
+    }
+
+  },
   data () {
     return {
       file: null,
-
+      chunkSize: 0.5 * 0.24 * 1024,
       process: 0,
       process2: 0,
       process3: 0,
       hash: '',
       hash2: '',
       hash3: '',
-      chunks: []
+      chunks: [],
+      cancelStatus: false
     }
   },
   computed: {
+
     cubeWidth () {
       return Math.ceil(Math.sqrt(this.chunks.length)) * 100 // 每个方框16px
     },
@@ -81,12 +122,36 @@ export default {
   },
   async mounted () {
     await this.$http.get('/user/info')
+
     this.bindEvents()
   },
   beforeDestroy () {
-
+    // 消除监听器
   },
   methods: {
+    async uploadAgain () {
+      const { data: { uploaded, uploadedList } } = await this.$http.post(this.checkFileAction, { hash: this.hash, ext: this.file.name.split('.').pop() })
+      // 这里的初始化才是响应式的 你没在这里写process 后面就不会根据process响应式 这里因为set函数里面会深层递归设置响应式
+      if (uploaded) {
+        // 秒传
+        return this.$message.success('已存在文件,秒传成功')
+      }
+      // 没完全上传文件 或者没上传过文件
+
+      this.chunks = this.chunks.map((chunk, index) => {
+        const name = this.hash + '-' + index
+        return {
+          ...chunk,
+          process: uploadedList.includes(name) ? 100 : 0
+        }
+      })
+      this.cancelStatus = false
+      // 已经上传过的chunks不需要上传
+      await this.uploadChunks(this.chunks, uploadedList)
+    },
+    cancel () {
+      this.source.cancel()
+    },
     blobToString (blob) {
       //! 需要异步执行函数都封装在promise里面，然后reslove拿结果  或者定义一个async 函数 里面await去等待结果
       return new Promise((resolve) => {
@@ -123,7 +188,7 @@ export default {
       const start = await this.blobToString(file.slice(0, 2))
       const end = await this.blobToString(file.slice(-2, len))
 
-      const isJpg = (start === 'FF 08') && (end === 'FF D9')
+      const isJpg = (start === 'FF D8') && (end === 'FF D9')
       return isJpg
     },
     bindEvents () {
@@ -158,17 +223,19 @@ export default {
       return chunks
     },
     calculateHashWorker (chunks) {
-      const worker = new Worker('/hash.js')
-      worker.postMessage({
-        chunks
-      })
-      worker.onmessage = (e) => {
-        const { process, hash } = e.data
-        this.process = process
-        if (hash) {
-          this.hash = hash
+      return new Promise((resolve, reject) => {
+        const worker = new Worker('/hash.js')
+        worker.postMessage({
+          chunks
+        })
+        worker.onmessage = (e) => {
+          const { process, hash } = e.data
+          this.process = process
+          if (hash) {
+            resolve(hash)
+          }
         }
-      }
+      })
     },
     calculateHashIdel (chunks) {
       const spark = new sparkMD5.ArrayBuffer()
@@ -183,23 +250,26 @@ export default {
           reader.readAsArrayBuffer(blobs)
         })
       }
-      const wookloop = async (deadline) => {
-        while (count < chunks.length && deadline.timeRemaining() > 1) {
-          await appendSpark(chunks[count].file)
-          count++
+      return new Promise((resolve, reject) => {
+        const wookloop = async (deadline) => {
+          while (count < chunks.length && deadline.timeRemaining() > 1) {
+            await appendSpark(chunks[count].file)
+            count++
 
-          if (count < chunks.length) {
-            this.process2 = Number((100 * count / chunks.length).toFixed(2))
+            if (count < chunks.length) {
+              this.process = Number((100 * count / chunks.length).toFixed(2))
+            }
+          }
+          if (count === chunks.length) {
+            this.process = 100.00
+            const hash = spark.end()
+            resolve(hash)
+          } else {
+            window.requestIdleCallback(wookloop)
           }
         }
-        if (count === chunks.length) {
-          this.process2 = 100.00
-          this.hash2 = spark.end()
-        } else {
-          window.requestIdleCallback(wookloop)
-        }
-      }
-      window.requestIdleCallback(wookloop)
+        window.requestIdleCallback(wookloop)
+      })
     },
     calcHashSample (file) {
       // 布隆过滤器 hash一样可能是同一个文件 但是不一样一定不是同一个文件 这个数据结构方便用来查找
@@ -231,13 +301,13 @@ export default {
         reader.readAsArrayBuffer(new Blob(chunks))
         reader.onload = (e) => {
           spark.append(e.target.result)
-          this.process3 = 100
+          this.process = 100
           resolve(spark.end())
         }
       })
     },
     async mergeChunks () {
-      await this.$http.post('merge', { ext: this.file.name.split('.').pop(), size: CHUNK_SIZE, hash: this.hash }) // 给后缀名和区块的size
+      await this.$http.post(this.mergeAction, { ext: this.file.name.split('.').pop(), size: this.chunkSize, hash: this.hash }) // 给后缀名和区块的size
     },
     async uploadChunks (chunks, uploadedList) {
       // !已经上传过的chunks不需要上传
@@ -258,15 +328,20 @@ export default {
       })) */
       // 一次性全部发出 有些在pending 这些都会申请tcp 申请tcp会很慢 是同时发起得upload导致卡 而不是多
       try {
-        await this.sendRequest(chunksFormData)
+        this.CancelToken = axios.CancelToken
+        this.source = this.CancelToken.source()
+        await this.sendRequest(chunksFormData, this.paralleLimit)
         // 所有切片传成功才会merge 才会有最后得文件 没全部时不会走到这得
         await this.mergeChunks()
       } catch (e) {
-        this.$message.success('文件重传次数大于3次')
+        if (axios.isCancel(e)) {
+          this.$message.success('取消上传')
+          this.cancelStatus = true
+        } else { this.$message.success('文件重传大于三次') }
       }
     },
     // 限制并发数量
-    sendRequest (chunksFormData, limit = 4) {
+    sendRequest (chunksFormData, limit) {
       // !一个数组，长度是limit，完成一个干掉一个 失败就重传,数组长度一直保持为limit
       return new Promise((resolve, reject) => {
         const len = chunksFormData.length
@@ -275,11 +350,14 @@ export default {
         const start = async () => {
           if (isStop) { return }
           const chunkForm = chunksFormData.shift() // 弹出一个task
+          if (!chunkForm) { return }
           counter += 1
           try {
-            await this.$http.post('/uploadfile', chunkForm.form, {
+            await this.$http.post(this.uploadAction, chunkForm.form, {
+              cancelToken: this.source.token,
               onUploadProgress: (progress) => {
                 // 直接取map的Index 永远是从0-len 永远都是前几个chunk在上传 我们应该取得是切块时候得index 那个才是chunk得唯一标识
+
                 this.chunks[chunkForm.index].process = parseInt(((progress.loaded / progress.total) * 100).toFixed(2))
               }
             })
@@ -292,7 +370,7 @@ export default {
           } catch (e) {
             // 当前区块上传报错则变红
             this.chunks[chunkForm.index].process = -1
-            if (chunkForm.errorCount < 3) {
+            if (chunkForm.errorCount < this.errorLimit) {
               chunkForm.errorCount++
               chunksFormData.unshift(chunkForm) // 插入这个坏的
               start() // 再次启动
@@ -314,19 +392,26 @@ export default {
     },
     async uploadFile () {
       if (!this.file) { return }
-      if (!this.isImage(this.file)) {
+
+      if (this.typeLimit && !await this.isImage(this.file)) {
         alert('文件格式不对')
         return
       }
-      const chunks = this.createFileChunk(this.file, CHUNK_SIZE)
+      this.chunkSize = Math.floor((this.file.size) / 10)
+      const chunks = this.createFileChunk(this.file, this.chunkSize)
 
       /* this.calculateHashWorker(chunks) */
       /* /* this.calculateHashIdel(chunks) */
-
-      this.hash = await this.calcHashSample(this.file) // 损失一部分精度去换取效率
-
+      if (this.type === 1) {
+        this.hash = await this.calculateHashWorker(chunks) // 损失一部分精度去换取效率
+      } else if (this.type === 2) {
+        this.hash = await this.calculateHashIdel(chunks) // 损失一部分精度去换取效率
+      } else {
+        this.hash = await this.calcHashSample(this.file)
+      }
+      console.log('this.hash', this.hash)
       // 计算完hash 用这个hash去问后台这个文件上传过吗
-      const { data: { uploaded, uploadedList } } = await this.$http.post('/checkFile', { hash: this.hash, ext: this.file.name.split('.').pop() })
+      const { data: { uploaded, uploadedList } } = await this.$http.post(this.checkFileAction, { hash: this.hash, ext: this.file.name.split('.').pop() })
       // 这里的初始化才是响应式的 你没在这里写process 后面就不会根据process响应式 这里因为set函数里面会深层递归设置响应式
       if (uploaded) {
         // 秒传
---
+++ pages/upload.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="upload">
+    <Upload />
+  </div>
+</template>
+
+<script>
+import Upload from '../components/Upload.vue'
+
+export default {
+  components: { Upload },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
